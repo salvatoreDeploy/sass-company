ae97c732159a463d432910f5786829d21a9300b6
Henrique J. Araujo <liderhenrique@gmail.com>
2024-08-07 08:53:59 -0300

    Page signin

---
+++ apps/web/src/app/auth/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Create Next App',
+}
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4">
+      <div className="w-full max-w-xs">{children}</div>
+    </div>
+  )
+}
