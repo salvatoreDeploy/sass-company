4044f38b1feeb5de54363afb13694b8193aa1d76
Henrique J. Araujo <liderhenrique@gmail.com>
2024-08-08 08:25:58 -0300

    Pages: Sign-up, forgot-password

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -10,6 +10,11 @@ export default function SignInPage() {
   return (
     <form action="" className="space-y-4">
       <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
+
+      <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
       </div>
@@ -17,28 +22,30 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
+      </div>
 
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
       </div>
 
       <Button type="submit" className="w-full">
-        Sign in with e-mail
+        Create account
       </Button>
 
       <Button variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
 
       <Separator />
 
       <Button type="submit" variant="outline" className="w-full">
         <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
-        Sigin in with GitHub
+        Sigin up with GitHub
       </Button>
     </form>
   )
