c178d815e4937f856310246b28691e7cf11d1110
Henrique J. Araujo <liderhenrique@gmail.com>
2024-11-15 13:10:16 -0300

    Alteração de header

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -17,10 +17,5 @@ export default function AppLayout({
     redirect('/auth/sign-in')
   }
 
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
-  )
+  return <>{children}</>
 }
