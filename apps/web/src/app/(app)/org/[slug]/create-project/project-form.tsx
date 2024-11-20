75aeacf24e68b730f0301b2e09fe323c91cfea2a
salvatoreDeploy <liderhenrique@gmail.com>
2024-11-20 13:44:03 -0300

    component: project-form

---
+++ apps/web/src/app/(app)/org/[slug]/create-project/project-form.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useFormState } from '@/hooks/use-form-state'
+import { useRouter } from 'next/router'
+import { createProjectAction } from './actions'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { AlertTriangle, Loader2 } from 'lucide-react'
+import { Textarea } from '@/components/ui/textarea'
+
+export function ProjectForm() {
+  const [{ errors, message, success }, handleSubmit, isPending] =
+    useFormState(createProjectAction)
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      {success === false && message && (
+        <Alert variant={'destructive'}>
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Save project failed</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      {success === true && message && (
+        <Alert variant={'success'}>
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Success!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      <div className="space-y-1">
+        <Label htmlFor="name">Project Name</Label>
+        <Input name="name" id="name" />
+
+        {errors?.name && (
+          <p className="flex items-center justify-start text-xs font-medium text-red-500 dark:text-red-400">
+            <AlertTriangle className="size-3" />
+            {errors.name[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="description">Description</Label>
+        <Textarea name="description" id="description" />
+
+        {errors?.description && (
+          <p className="flex items-center justify-start text-xs font-medium text-red-500 dark:text-red-400">
+            <AlertTriangle className="size-3" />
+            {errors.description[0]}
+          </p>
+        )}
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size-4 animate-spin" />
+        ) : (
+          'Save project'
+        )}
+      </Button>
+    </form>
+  )
+}
